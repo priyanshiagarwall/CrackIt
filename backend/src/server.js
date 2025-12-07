2ac55ee1927d8b4f50e6703a9c363954b5bedf6f
priyanshiagarwall <priyanshiagarwalxc28@gmail.com>
2025-12-07 07:03:50 +0530

    server side auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,14 @@ import path from "path"
 import cors from "cors"
 
 import {serve} from "inngest/express"
+import { clerkMiddleware } from "@clerk/express";
+
 
 
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import { inngest,functions } from "./lib/inngest.js";
+import chatRoutes from "./routes/chatRoutes.js"
 
 
 
@@ -21,6 +24,8 @@ app.use(express.json())
 
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat",chatRoutes)
+app.use(clerkMiddleware()); 
 
 
 app.get("/health", (req, res) => {
