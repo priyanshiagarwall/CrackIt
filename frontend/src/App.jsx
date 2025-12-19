f1886815d6008ca10ba18c2e2c9d9fa9bf7e3ba0
priyanshiagarwall <priyanshiagarwalxc28@gmail.com>
2025-12-19 12:30:34 +0530

    frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,21 @@
 
-import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from '@clerk/clerk-react'
+import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton, useUser } from '@clerk/clerk-react'
+import { Navigate, Route, Routes } from 'react-router'
+import HomePage from './pages/HomePage'
+import ProblemsPage from './pages/ProblemsPage'
+
 
 function App() {
-  
+    const { isSignedIn } = useUser();
 
   return (
-    <>
-      <h1>Welcome to the app</h1>
-      <SignedOut>
-        <SignInButton mode='modal'>
-          <button>
-            Login
-          </button>
-        </SignInButton>
-      </SignedOut>
-
-      <SignedIn>
-        <SignOutButton  />
-      </SignedIn>
-
-      <UserButton/>
+    <Routes>
       
-    </>
+
+      <Route path='/' element={<HomePage/>} />
+      <Route path='/problems' element={isSignedIn?<ProblemsPage/>:<Navigate to={"/"}/>} />
+      
+    </Routes>
   )
 }
 
---
+++ frontend/src/pages/ProblemsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProblemsPage() {
+  return (
+    <div>
+      Problems Page
+    </div>
+  )
+}
+
+export default ProblemsPage
